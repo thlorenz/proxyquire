dc220f3f030a68264bb06e3b23985b535a1e5d31
Thorsten Lorenz <thlorenz@gmx.de>
2012-07-05 08:08:13 -0400

    tested deleting overrides in strict mode

--- test/proxyquire.js
+++ test/proxyquire.js
@@ -67,99 +67,93 @@ describe('module overrides', function () {
         path.basename('/path/a.txt')
       }).should.throw(/has no method.*basename/);
     })
-    
   })
 })
 
 
 describe('module path resolution', function () {
   describe('when I require a module that is part of my project', function () {
-      it('resolves that module', function () {
-        // foo requires bar using proxyquire
-        require('./samples/foo').gotoBar().should.eql('you are a drunk');
-      })     
+    it('resolves that module', function () {
+      // foo requires bar using proxyquire
+      require('./samples/foo').gotoBar().should.eql('you are a drunk');
+    })     
   })
 })
 
-describe('overriding and removing overrides incrementally', function () {
-  var path;
-  function addIncrementally() {
+describe('adding and removing overrides incrementally', function () {
+  describe('when I override path.basename to return "1"', function () {
+
+    var path;
+    beforeEach(function () {
       proxyquire()
         .reset()
         .add({
           path: { 
             basename: function () { return 1; }
           }
-        })
-        .add({
-          path: { 
-            extname: function () { return 2; }
-          }
-        });
-
+      })
       path = proxyquire('path');
-    }
-
-  describe('when I override path.basename to return "1" and then path.extname to return "2"', function () {
-    before(addIncrementally);
+    });
 
     it('path.basename("x") returns 1', function () {
       path.basename('x').should.eql(1);  
     })
     
-    it('path.extname("x") returns 2', function () {
-      path.extname('x').should.eql(2);  
+    it('path.extname("x") returns ""', function () {
+      path.extname('x').should.eql('');  
     })
 
-    describe('and then I remove path.basename override', function () {
-      before(function () {
-        addIncrementally();
-        proxyquire().del({ path: 'basename' });
+    describe('and then path.extname to return "2"', function () {
+
+      beforeEach(function () {
+        proxyquire().add({
+          path: { 
+            extname: function () { return 2; }
+          }
+        })
       });
 
-      it('path.basename("x") returns x', function () {
-        path.basename('x').should.eql('x');  
+      it('path.basename("x") returns 1', function () {
+        path.basename('x').should.eql(1);  
       })
 
       it('path.extname("x") returns 2', function () {
         path.extname('x').should.eql(2);  
       })
-    })
 
-    describe('and then I remove path.basename  and path.extname overrides', function () {
-      before(function () {
-        addIncrementally();
-        proxyquire().del({ path: ['basename', 'extname' ] });
-      });
+      describe('and then I remove path.basename override', function () {
+        beforeEach(function () {
+          proxyquire().del({ path: 'basename' });
+        });
 
-      it('path.basename("x") returns x', function () {
-        path.basename('x').should.eql('x');  
-      })
+        it('path.basename("x") returns x', function () {
+          path.basename('x').should.eql('x');  
+        })
 
-      it('path.extname("x") returns ""', function () {
-        path.extname('x').should.eql('');  
-      })
-    })
+        it('path.extname("x") returns 2', function () {
+          path.extname('x').should.eql(2);  
+        })
 
-    describe('and then I remove path module entirely', function () {
-      before(function () {
-        addIncrementally();
-        proxyquire().del('path');
-      });
+        describe('and then I remove path.extname override', function () {
+          beforeEach(function () {
+            proxyquire().del({ path: 'extname' });
+          });
 
-      it('path.basename("x") returns x', function () {
-        path.basename('x').should.eql('x');  
-      })
+          it('path.basename("x") returns x', function () {
+            path.basename('x').should.eql('x');  
+          })
 
-      it('path.extname("x") returns ""', function () {
-        path.extname('x').should.eql('');  
+          it('path.extname("x") returns ""', function () {
+            path.extname('x').should.eql("");  
+          })
+        })
       })
     })
   })
 })
 
 
-describe('local module removing overrides,', function () {
+describe('removing overrides for local module,', function () {
 
   var foo;
 
@@ -243,6 +237,36 @@ describe('local module removing overrides,', function () {
       it('drinksOnMe returns original', function () {
         foo.throwRound().should.eql('you are rich');  
       })
+    })
+  })
+})
+
+describe('when I override path.basename to return 1 in strict mode', function () {
+  
+  var path;
+  beforeEach(function () {
+    proxyquire({
+      path: { 
+          basename: function () { return 1; }
+        , __proxyquire: { strict: true }
+      }
+    })
+    path = proxyquire('path');
+  });
+
+  it('path.basename("x") returns 1', function () {
+    path.basename('x').should.eql(1);  
+  })
+
+  describe('and then remove path.basename override', function () {
+    beforeEach(function () {
+      proxyquire().del({ path: 'basename' });
+    })
+
+    it('path.basename("/path/a.txt" throws "has no method basename"', function () {
+      (function () {
+        path.basename('/path/a.txt')
+      }).should.throw(/has no method.*basename/);
     })
   })
 })
