4ba64302e9f152f1e61e34c113d777a1f8797e7c
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-05 20:35:15 -0500

    added tests that outline current bug

---
+++ test/proxyquire-bug.js
@@ -0,0 +1,50 @@
+'use strict';
+/*jshint asi:true */
+/*global describe before beforeEach it */
+var proxyquire = require('..')
+  , someArray = []
+
+function throwsUndefinedIsNotFunction() {
+  proxyquire('./samples/foo', { 
+      fs :  { }
+    , fn :  function () { console.log(); }
+  })
+}
+
+function alsoThrowsUndefinedIsNotFunction() {
+  proxyquire('./samples/foo', { 
+      fs :  { }
+    , fn :  function () { someArray.push(1) }
+  })
+}
+
+function fineSinceNoConsoleLog() {
+  proxyquire('./samples/foo', { 
+      fs :  { }
+    , fn :  function () { return 1 + 1 }
+  })
+}
+
+function fineSinceOnlyOneStubbedProperty() {
+  proxyquire('./samples/foo', { 
+    fn :  function () { console.log(); }
+  })
+}
+
+function fineSinceFunctionIsFirst() {
+  proxyquire('./samples/foo', { 
+      fn :  function () { console.log(); }
+    , fs :  { }
+  })
+}
+
+describe('when I require problematic stubs', function () {
+  
+  it('none of them should throw', function () {
+    fineSinceOnlyOneStubbedProperty()
+    fineSinceNoConsoleLog()
+    fineSinceFunctionIsFirst()
+    // alsoThrowsUndefinedIsNotFunction()
+    // throwsUndefinedIsNotFunction()
+  })
+})
