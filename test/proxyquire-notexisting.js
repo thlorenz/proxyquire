0297d28db7d02475ff1700820f5e25fc47870df2
Brian Valerius <bvalerius@users.noreply.github.com>
2019-08-08 14:21:52 -0500

    fix: allow non-existent stub path when @noCallThru is set at stub level (#245)

--- test/proxyquire-notexisting.js
+++ test/proxyquire-notexisting.js
@@ -4,6 +4,7 @@ var assert = require('assert')
 var proxyquire = require('..')
 var path = require('path')
 var fooPath = path.join(__dirname, './samples/notexisting/foo.js')
+var fooRelativePath = path.join(__dirname, './samples/notexisting/foo-relative.js')
 
 describe('When resolving foo that requires stubbed /not/existing/bar.json', function () {
   it('throws an error', function () {
@@ -32,5 +33,14 @@ describe('When resolving foo that requires stubbed /not/existing/bar.json with n
     })
     assert.strictEqual(foo.config, 'bar\'s config')
     proxyquire.callThru()
+  })
+})
+
+describe('When resolving foo-relative that requires relative stubbed ../not/existing/bar.json with @noCallThru', function () {
+  it('resolves foo-relative with stubbed bar', function () {
+    var fooRelative = proxyquire(fooRelativePath, {
+      '../not/existing/bar.json': { config: 'bar\'s config', '@noCallThru': true }
+    })
+    assert.strictEqual(fooRelative.config, 'bar\'s config')
   })
 })
